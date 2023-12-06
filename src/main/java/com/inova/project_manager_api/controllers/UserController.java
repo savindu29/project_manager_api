2271b97fd54cd965884f86a2ffd176c75e3a6e07
Hashimi <COHDSE221F-043@student.nibm.lk>
2023-12-06 18:39:44 +0530

    Acc request

--- src/main/java/com/inova/project_manager_api/controllers/UserController.java
+++ src/main/java/com/inova/project_manager_api/controllers/UserController.java
@@ -5,12 +5,10 @@ import com.inova.project_manager_api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/user-requests")
 public class UserController {
     @Autowired
