1659f91ffc5d0a3aef3a5eaa693a7cd48a54caa9
Hashimi <COHDSE221F-043@student.nibm.lk>
2023-12-11 11:22:04 +0530

    Login Error

--- src/main/java/com/inova/project_manager_api/controllers/AuthenticationController.java
+++ src/main/java/com/inova/project_manager_api/controllers/AuthenticationController.java
@@ -5,12 +5,10 @@ import com.inova.project_manager_api.dto.request.SigninRequest;
 import com.inova.project_manager_api.dto.response.JwtAuthenticationResponse;
 import com.inova.project_manager_api.services.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
