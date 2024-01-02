aac0adafbb53813eaf1ac27a1848311f388be009
Hashimi <COHDSE221F-043@student.nibm.lk>
2024-01-02 19:32:08 +0530

    searchPotentialResources

--- src/main/java/com/inova/project_manager_api/controllers/ResourceController.java
+++ src/main/java/com/inova/project_manager_api/controllers/ResourceController.java
@@ -5,23 +5,20 @@ import com.inova.project_manager_api.utils.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/v1/admin")
-@CrossOrigin(origins = "http://localhost:3000")
+
 public class ResourceController {
-
     @Autowired
-    private ResourceService resourceService;
+    private ResourceService resourceService ;
 
-    @GetMapping("/employees/notAllocatedToProject")
-    public ResponseEntity<StandardResponse> getEmployeesNotAllocatedToProject(@RequestParam int projectId) {
+    public ResponseEntity<StandardResponse> searchPotentialResources()
+
+    {
         return new ResponseEntity<>(
-                resourceService.getEmployeesAndProjectsNotAllocatedToProject((long) projectId),
+
+                resourceService. searchPotentialResources (),
                 HttpStatus.OK
         );
     }
-
 
 }
--- src/main/java/com/inova/project_manager_api/services/ResourceService.java
+++ src/main/java/com/inova/project_manager_api/services/ResourceService.java
@@ -4,6 +4,4 @@ import com.inova.project_manager_api.utils.StandardResponse;
 
 public interface ResourceService {
     StandardResponse searchPotentialResources();
-
-    StandardResponse getEmployeesAndProjectsNotAllocatedToProject(Long projectId);
 }
