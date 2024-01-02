93aa49e0d03e54c7188d14f13bd1c4eb9032dfe4
Hashimi <COHDSE221F-043@student.nibm.lk>
2024-01-02 19:32:08 +0530

    searchPotentialResources

---
+++ src/main/java/com/inova/project_manager_api/services/ResourceService.java
@@ -0,0 +1,7 @@
+package com.inova.project_manager_api.services;
+
+import com.inova.project_manager_api.utils.StandardResponse;
+
+public interface ResourceService {
+    StandardResponse searchPotentialResources();
+}
