4c0575d94d894b0088b6fa53199708647d030926
madhuni.tharukshi <madhuni.tharukshi@inovaitsys.com>
2024-01-03 15:13:05 +0530

    this commit for project allocation and available percentage calculation

---
+++ src/main/java/com/inova/project_manager_api/controllers/ProjectResourceController.java
@@ -0,0 +1,47 @@
+package com.inova.project_manager_api.controllers;
+
+import com.inova.project_manager_api.dto.request.ResourceRequestDto;
+import com.inova.project_manager_api.dto.response.ProjectResourceResponseDto;
+import com.inova.project_manager_api.dto.response.ResourceAllocationResponseDto;
+import com.inova.project_manager_api.services.ProjectResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/project")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ProjectResourceController {
+
+    @Autowired
+    private ProjectResourceService projectResourceService;
+
+    @PostMapping("/available_percentages")
+    public ResponseEntity<List<ProjectResourceResponseDto>> projectResourcePercentage(@RequestBody ResourceRequestDto request) {
+
+        return new ResponseEntity<>(
+
+                projectResourceService.availablePercentage(request),
+                HttpStatus.OK
+        );
+
+    }
+
+    @PostMapping("/project_allocation")
+    public ResponseEntity<List<ResourceAllocationResponseDto>> projectAllocation(@RequestBody ResourceRequestDto request) {
+
+        return new ResponseEntity<>(
+
+                projectResourceService.projectAllocation(request),
+                HttpStatus.OK
+        );
+
+    }
+
+
+
+
+}
