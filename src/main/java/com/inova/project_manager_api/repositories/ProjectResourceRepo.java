d509e358ba5225e2af5fc42a4cad6bb1b8b50d9e
Hashimi <COHDSE221F-043@student.nibm.lk>
2024-01-03 14:08:55 +0530

    /employees/notAllocatedToProject

--- src/main/java/com/inova/project_manager_api/repositories/ProjectResourceRepo.java
+++ src/main/java/com/inova/project_manager_api/repositories/ProjectResourceRepo.java
@@ -1,25 +1,14 @@
 package com.inova.project_manager_api.repositories;
 
 import com.inova.project_manager_api.entities.Employee;
-import com.inova.project_manager_api.entities.Project;
-import com.inova.project_manager_api.entities.ProjectResource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProjectResourceRepo extends JpaRepository<ProjectResource, Long> {
+public interface ProjectResourceRepo extends JpaRepository<Employee, Integer> {
 
-    // Find employees not allocated to the specified project and approved
-    @Query("SELECT pr.employee FROM ProjectResource pr WHERE pr.project.id = :projectId AND pr.approved = true")
-    List<Employee> findEmployeesNotAllocatedToProject(@Param("projectId") Long projectId);
-
-    // Find projects allocated to the employee excluding the specified project and approved
-    @Query("SELECT pr.project FROM ProjectResource pr WHERE pr.employee.id = :employeeId AND pr.project.id <> :excludedProjectId AND pr.approved = true")
-    List<Project> findProjectsAllocatedToEmployeeExcludingGivenProject(@Param("employeeId") Long employeeId, @Param("excludedProjectId") Long excludedProjectId);
-
-    // Find pending projects for the specified employee and project
-    @Query("SELECT pr.project FROM ProjectResource pr WHERE pr.employee.id = :employeeId AND pr.project.id = :projectId AND pr.approved = false")
-    List<Project> findPendingProjectsForEmployeeAndProject(@Param("employeeId") Long employeeId, @Param("projectId") Long projectId);
+    @Query("SELECT e FROM Employee e WHERE e.id NOT IN (SELECT pr.employee.id FROM ProjectResource pr WHERE pr.project.id = :projectId)")
+    List<Employee> findEmployeesNotAllocatedToProject(@Param("projectId") int projectId);
 }
--- src/main/java/com/inova/project_manager_api/services/impl/ResourceServiceImpl.java
+++ src/main/java/com/inova/project_manager_api/services/impl/ResourceServiceImpl.java
@@ -1,17 +1,36 @@
 package com.inova.project_manager_api.services.impl;
 
+import com.inova.project_manager_api.dto.request.EmployeeNameDto;
+import com.inova.project_manager_api.repositories.ProjectResourceRepo;
 import com.inova.project_manager_api.services.ResourceService;
 import com.inova.project_manager_api.utils.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ResourceServiceImpl implements ResourceService {
 
+    @Autowired
+    private ProjectResourceRepo projectResourceRepo;
+
     @Override
     public StandardResponse searchPotentialResources() {
-        return null;
+        // Logic for searching potential resources
+        // ...
 
-        ///logic here
-        //
+        return null;
+    }
+
+    @Override
+    public StandardResponse getEmployeesNotAllocatedToProject(int projectId) {
+        List<EmployeeNameDto> employeeNames = projectResourceRepo.findEmployeesNotAllocatedToProject(projectId)
+                .stream()
+                .map(EmployeeNameDto::new)
+                .collect(Collectors.toList());
+
+        return new StandardResponse("Success", employeeNames);
     }
 }
