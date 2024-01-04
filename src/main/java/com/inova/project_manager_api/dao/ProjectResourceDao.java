cbac21d573cd47352a71a0e39d60be4012448e79
RoshenH <roshen.20210497@gmail.com>
2024-01-04 11:41:19 +0530

    Merge remote-tracking branch 'origin/dev_roshen' into dev_roshen

---
+++ src/main/java/com/inova/project_manager_api/dao/ProjectResourceDao.java
@@ -0,0 +1,9 @@
+package com.inova.project_manager_api.dao;
+
+import com.inova.project_manager_api.dto.request.ProjectResourceDto;
+
+import java.util.List;
+
+public interface ProjectResourceDao {
+    List<ProjectResourceDto>ResourceList();
+}
