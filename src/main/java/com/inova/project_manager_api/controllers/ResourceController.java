1df100be4d5287b3f6316e366d7c16e25f0054a6
savindu29 <1savindupanagoda@gmail.com>
2024-01-07 10:51:56 +0530

    developed resource search by skill

--- src/main/java/com/inova/project_manager_api/controllers/ResourceController.java
+++ src/main/java/com/inova/project_manager_api/controllers/ResourceController.java
@@ -1,11 +1,16 @@
 package com.inova.project_manager_api.controllers;
 
+import com.inova.project_manager_api.dto.request.ProjectRequestDto;
+import com.inova.project_manager_api.dto.request.ResourceSpecificationRequestDto;
 import com.inova.project_manager_api.services.ResourceService;
 import com.inova.project_manager_api.utils.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.SplittableRandom;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -19,6 +24,14 @@ public class ResourceController {
     public ResponseEntity<StandardResponse> getEmployeesNotAllocatedToProject(@RequestParam int projectId) {
         return new ResponseEntity<>(
                 resourceService.getEmployeesAndProjectsNotAllocatedToProject( projectId),
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/employees/employees-by-skill")
+    public ResponseEntity<StandardResponse> getEmployeesBySkill(@RequestBody ResourceSpecificationRequestDto request){
+        return new ResponseEntity<>(
+                resourceService.getEmployeesBySkill(request),
                 HttpStatus.OK
         );
     }
--- src/main/java/com/inova/project_manager_api/services/ResourceService.java
+++ src/main/java/com/inova/project_manager_api/services/ResourceService.java
@@ -1,9 +1,12 @@
 package com.inova.project_manager_api.services;
 
+import com.inova.project_manager_api.dto.request.ResourceSpecificationRequestDto;
 import com.inova.project_manager_api.utils.StandardResponse;
 
 public interface ResourceService {
     StandardResponse searchPotentialResources();
 
     StandardResponse getEmployeesAndProjectsNotAllocatedToProject(int projectId);
+
+    StandardResponse getEmployeesBySkill(ResourceSpecificationRequestDto request);
 }
